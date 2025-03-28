07a3bb5bd89dc59970c8fbc58438c872a19e828b
mohamad-javad-jokar-sarhangi <mjavadx3@gmail.com>
2025-03-28 17:37:36 +0330

    make choice for role in front

--- users/forms.py
+++ users/forms.py
@@ -2,13 +2,23 @@ from django import forms
 from .models import UserNotRegister
 
 class UserNotRegisterForm(forms.ModelForm):
+    ROLE_CHOICES = [
+        ("روستایی", "روستایی"),
+        ("راننده", "راننده"),
+        ("فروشنده", "فروشنده"),
+        ("دهیار-شورا", "دهیار-شورا"),
+    ]
+
     class Meta:
         model = UserNotRegister
         fields = ['name', 'phone', 'role']
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'نام'}),
             'phone': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'شماره تماس'}),
-            'role': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'نقش'}),
         }
-
-
+    
+    # تغییر نقش به یک فیلد انتخابی
+    role = forms.ChoiceField(choices=ROLE_CHOICES, widget=forms.Select(attrs={
+        'class': 'form-control',
+        'placeholder': 'نقش',
+    }))
