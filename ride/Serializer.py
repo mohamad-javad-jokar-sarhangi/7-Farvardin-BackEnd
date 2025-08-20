c7690700f0a56c05b6d385b1880f8bcb0f746167
mohamad-javad-jokar-sarhangi <mjavadx3@gmail.com>
2025-08-21 02:24:31 +0330

    changed serializer api according to models

--- ride/Serializer.py
+++ ride/Serializer.py
@@ -4,8 +4,8 @@ from .models import TripRequest, DriverQueue, DriverAcceptance
 class TripRequestSerializer(serializers.ModelSerializer):
     class Meta:
         model = TripRequest
-        fields = '__all__'
-        read_only_fields = ['status', 'created_at']
+        fields = '__all__'  # همه فیلدهای مدل
+        # هیچ فیلدی read_only نیست مگر بخوای passenger رو دستی ست کنی در view
 
 
 class DriverQueueSerializer(serializers.ModelSerializer):
